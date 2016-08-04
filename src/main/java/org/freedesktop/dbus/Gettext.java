f10c07bd7c63996daabc027515a46146c7f46643
Joe Morales <joseph.morales@flukenetworks.com>
2016-08-04 11:09:44 -0600

    dbus-java changes for Android

--- src/main/java/org/freedesktop/dbus/Gettext.java
+++ src/main/java/org/freedesktop/dbus/Gettext.java
@@ -22,9 +22,12 @@ import java.util.ResourceBundle;
 
 public class Gettext
 {
-   private static ResourceBundle myResources =
-      ResourceBundle.getBundle("dbusjava_localized");
+//   private static ResourceBundle myResources =
+//      ResourceBundle.getBundle("dbusjava_localized");
    public static String _(String s) {
-      return myResources.getString(s);
+//      return myResources.getString(s);
+
+      // Resources not setup for Android use
+      return s;
    }
 }
